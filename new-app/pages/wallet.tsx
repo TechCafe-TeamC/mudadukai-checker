2cd6ffe5399c5048b68ac30eb344bb4e4fe47616
NewNomad <tosiki1997@gmail.com>
2022-12-21 16:32:29 +0900

    add ShowModal.tsx

--- new-app/pages/wallet.tsx
+++ new-app/pages/wallet.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WalletNav from '../components/WalletNav'
 import { motion } from 'framer-motion'
 import FullCalendar from "@fullcalendar/react"
 import dayGridPlugin from "@fullcalendar/daygrid"
 import jaLocale from "@fullcalendar/core/locales/ja"
 import styled from "@emotion/styled"
+import { Button, Modal } from 'react-bootstrap'
+import { ShowModal } from '../components/SHowModal'
 
 const wallet = () => {
+  const [showModal, setshowModal] = useState<boolean>(false)
+  const OpenModal = () => setshowModal(true)
+  const CloseModal = () => setshowModal(false)
+
+
   return (
     <motion.div
       initial={{ y: 1000, opacity: 0.5 }}
@@ -14,14 +21,7 @@ const wallet = () => {
       exit={{ y: 1000, opacity: 0.5 }}
       transition={{ ease: "easeInOut", duration: 0.4 }}
     >
-      <StyleWrapper>
-        <FullCalendar
-          plugins={[dayGridPlugin]}
-          initialView="dayGridMonth"
-          locales={[jaLocale]}
-          locale="ja"
-        />
-      </StyleWrapper>
+
       <div className='
         flex
         justify-left
@@ -29,9 +29,24 @@ const wallet = () => {
         h-screen'
       >
 
-        <WalletNav />
+        <WalletNav OnClick={OpenModal} />
       </div>
-    </motion.div>
+      {/* <StyleWrapper>
+          <FullCalendar
+            plugins={[dayGridPlugin]}
+            initialView="dayGridMonth"
+            locales={[jaLocale]}
+            locale="ja"
+          />
+        </StyleWrapper> */}
+      <ShowModal
+        showModal={showModal}
+        OpenModal={OpenModal}
+        CloseModal={CloseModal}
+        title="無駄遣いCalendar"
+
+      >aaaa</ShowModal>
+    </motion.div >
   )
 }
 
