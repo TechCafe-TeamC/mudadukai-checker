bfd6a82aac5f60c9e0906526134dec7a081282cf
NewNomad <tosiki1997@gmail.com>
2022-12-22 15:56:53 +0900

    add 500円が落ちて床に転がる

--- new-app/components/materials/Yen500.tsx
+++ new-app/components/materials/Yen500.tsx
@@ -1,17 +1,18 @@
 import { useLoader } from "@react-three/fiber"
 import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader"
 import { TextureLoader } from "three/src/loaders/TextureLoader"
-import { Physics, useSphere } from "@react-three/cannon"
+import { Physics, useBox, usePlane } from "@react-three/cannon"
 
 export const Yen500 = () => {
-  const [ref] = useSphere(() => ({
-    mass: 100,
-    // args:[5,5,5],
-    position: [0, 0, 0],
-    rotation: [-Math.PI / 2, 0, 0]
+  const [ref] = useBox(() => ({
+    mass: 1,
+    args: [2.3, 2.3, 0.1],
+    position: [0, 20, 0],
+    rotation: [0, 0, 0],
+
   }))
   const fbx = useLoader(FBXLoader, "./money24.fbx")
   const map = useLoader(TextureLoader, "./5_10_Roughness.png")
 
-  return <primitive object={fbx} map={map} ref={ref} args={[0.1, 0.1, 0.1]} />
+  return <primitive object={fbx} map={map} ref={ref} scale={0.1} />
 }
